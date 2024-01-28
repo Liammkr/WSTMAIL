a77c9ff5bbc83a73c7d4fba6a1c03640f786492a
Liam <102763734+Liammkr@users.noreply.github.com>
2024-01-27 22:15:05 -0800

    Selection Menu

--- main.py
+++ main.py
@@ -1,17 +1,47 @@
 import smtplib
 from email.message import EmailMessage
-a = 1
+from colorama import Fore
+from colorama import init
+init()
 t = 0
+print(
+    Fore.MAGENTA +
+    """                                                                                        
+                                                                                       
+                          .                .,                                ., G:        
+                         ;W               ,Wt j.                            ,Wt E#,    :
+            ;           f#E GEEEEEEEL    i#D. EW,                   ..     i#D. E#t  .GE
+          .DL         .E#f  ,;;L#K;;.   f#f   E##j                 ;W,    f#f   E#t j#K;
+  f.     :K#L     LWLiWW;      t#E    .D#i    E###D.              j##,  .D#i    E#GK#f  
+  EW:   ;W##L   .E#fL##Lffi    t#E   :KW,     E#jG#W;            G###, :KW,     E##D.   
+  E#t  t#KE#L  ,W#;tLLG##L     t#E   t#f      E#t t##f         :E####, t#f      E##Wi   
+  E#t f#D.L#L t#K:   ,W#i      t#E    ;#G     E#t  :K#E:      ;W#DG##,  ;#G     E#jL#D: 
+  E#jG#f  L#LL#G    j#E.       t#E     :KE.   E#KDDDD###i    j###DW##,   :KE.   E#t ,K#j
+  E###;   L###j   .D#j         t#E      .DW:  E#f,t#Wi,,,   G##i,,G##,    .DW:  E#t   jD
+  E#K:    L#W;   ,WK,          t#E        L#, E#t  ;#W:   :K#K:   L##,      L#, j#t     
+  EG      LE.    EG.            fE         jt DWi   ,KK: ;##D.    L##,       jt  ,;     
+  ;       ;@     ,               :                       ,,,      .,,                   
+                           
+                                                                              liamm - t.me/wst
+                                                                                        """
+)
+senderEmail = input(Fore.LIGHTBLUE_EX +"Sender Email: ") 
+senderPass = input(Fore.LIGHTBLUE_EX +"Sender App Password: ") 
+toEmail = input(Fore.LIGHTBLUE_EX +"Recieving Email: ") 
+subjectInput = input(Fore.LIGHTBLUE_EX +"Subject: ")
+bodyInput = input(Fore.LIGHTBLUE_EX +"Body: ")
+a = input(Fore.LIGHTBLUE_EX +"Ammount Of Emails: ")
 def email_alert(subject, body, to):
     msg = EmailMessage()
     msg.set_content(body)
     msg['subject'] = subject
     msg['to'] = to
 
-    user = "liamkrubin@gmail.com"
+    user = str(senderEmail)
     msg['from'] = user
-    password = "xbedywkocqyhkjnh"
+    password = str(senderPass)
     #vqkjdjnwtzyszfdi
+    #xbedywkocqyhkjnh
   
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
@@ -19,13 +49,11 @@ def email_alert(subject, body, to):
     server.send_message(msg)
 
     server.quit()
-
-toEmail= input("Recieving Email: ") 
-
-if toEmail.lower() == ' ': 
-   exit
-
-while a == 1:
-    email_alert("Not Liam","Hi",str(toEmail))
+aInt = int(a)
+while aInt > 0:
+    email_alert(str(subjectInput),str(bodyInput),str(toEmail))
     t+=1
-    print("Emails Sent: "+ str(t))
+    print(Fore.GREEN + "Emails Sent: "+ str(t)+ " STATUS: [201] ACCEPTED")
+    aInt-=1
+else:
+    print(Fore.YELLOW+"FINISHED")

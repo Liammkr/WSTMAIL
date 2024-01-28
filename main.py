ec029ba03da9ed769c918f978b7fbea69c7db4b4
Liam <102763734+Liammkr@users.noreply.github.com>
2024-01-27 21:12:40 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+mport smtplib
+from email.message import EmailMessage
+
+def email_alert(subject, body, to):
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['subject'] = subject
+    msg['to'] = to
+
+    user = "lr14@smmk12.org"
+    msg['from'] = user
+    password = "vqkjdjnwtzyszfdi"
+  
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(user, password)
+    server.send_message(msg)
+
+    server.quit()

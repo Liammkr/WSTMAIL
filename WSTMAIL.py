99dc499f000c47450cf095e38d26e060ac1e988e
Liam <102763734+Liammkr@users.noreply.github.com>
2024-01-28 19:52:05 -0800

    Update and rename main.py to WSTMAIL.py

--- WSTMAIL.py
+++ WSTMAIL.py
@@ -2,9 +2,13 @@ import smtplib
 from email.message import EmailMessage
 from colorama import Fore
 from colorama import init
+import os
+from time import sleep
 init()
-t = 0
-print(
+start = True
+while start == True:
+    os.system('cls')
+    print(
     Fore.MAGENTA +
     """                                                                                        
                                                                                        
@@ -25,30 +29,38 @@ print(
                                                                               liamm --
                                                                                         """
 )
-senderEmail = input(Fore.LIGHTBLUE_EX +"Sender Email: ") 
-senderPass = input(Fore.LIGHTBLUE_EX +"Sender App Password: ") 
-toEmail = input(Fore.LIGHTBLUE_EX +"Recieving Email: ") 
-subjectInput = input(Fore.LIGHTBLUE_EX +"Subject: ")
-bodyInput = input(Fore.LIGHTBLUE_EX +"Body: ")
-a = input(Fore.LIGHTBLUE_EX +"Ammount Of Emails: ")
-def email_alert(subject, body, to):
-    msg = EmailMessage()
-    msg.set_content(body)
-    msg['subject'] = subject
-    msg['to'] = to
-    user = str(senderEmail)
-    msg['from'] = user
-    password = str(senderPass)
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(user, password)
-    server.send_message(msg)
-    server.quit()
-aInt = int(a)
-while aInt > 0:
-    email_alert(str(subjectInput),str(bodyInput),str(toEmail))
-    t+=1
-    print(Fore.GREEN + "Emails Sent: "+ str(t)+ " STATUS: [201] ACCEPTED")
-    aInt-=1
-else:
-    print(Fore.YELLOW+"FINISHED")
+    senderEmail = input(Fore.LIGHTBLUE_EX +"Sender Email: ") 
+    senderPass = input(Fore.LIGHTBLUE_EX +"Sender App Password: ") 
+    toEmail = input(Fore.LIGHTBLUE_EX +"Recieving Email: ") 
+    subjectInput = input(Fore.LIGHTBLUE_EX +"Subject: ")
+    bodyInput = input(Fore.LIGHTBLUE_EX +"Body: ")
+    a = input(Fore.LIGHTBLUE_EX +"Ammount Of Emails: ")
+    def email_alert(subject, body, to):
+       msg = EmailMessage()
+       msg.set_content(body)
+       msg['subject'] = subject
+       msg['to'] = to
+       user = str(senderEmail)
+       msg['from'] = user
+       password = str(senderPass)
+       server = smtplib.SMTP("smtp.gmail.com", 587)
+       server.starttls()
+       server.login(user, password)
+       server.send_message(msg)
+       server.quit()
+    aInt = int(a)
+    t=0
+    while aInt > 0:
+        email_alert(str(subjectInput),str(bodyInput),str(toEmail))
+        t+=1
+        print(Fore.GREEN + "Emails Sent: "+ str(t)+ " STATUS: [201] ACCEPTED")
+        aInt-=1
+    else:
+        print(Fore.YELLOW+"FINISHED")
+        restart = input(Fore.LIGHTCYAN_EX+"More Emails (y/n)")
+        if restart == 'y':
+            start = True
+        else:
+            print(Fore.RED+"Closing Program in 2s")
+            sleep(2)
+            start = False
